a711ff84b5d99f8bfdef1eeb365d20811e4c5720
Guozv <1602427689@qq.com>
2025-10-29 16:07:05 +0800

    AT_codefestival_2016_final_f_Road_of_the_King

---
+++ 计数/AT_codefestival_2016_final_f_Road_of_the_King.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+//#pragma GCC optimize("Ofast")
+using namespace std;
+template<typename T>
+inline bool cmax(T&x,const T& y){return x<y?x=y,1:0;}
+template<typename T>
+inline bool cmin(T&x,const T& y){return y<x?x=y,1:0;}
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vector<int> > vii; 
+const int mod=1e9+7;
+inline void MOD(int&x){x-=mod,x+=x>>31&mod;}
+inline void MOD(ll& x){x-=mod,x+=x>>63&mod;}
+inline int add(int x,int y){MOD(x+=y);return x;}
+inline int mul(int x,int y){return 1ll*x*y%mod;}
+template<typename ... A>inline int mul(const int& x,const A&... p){return 1ll*x*mul(p...)%mod;}
+inline ll ksm(ll a,ll p=mod-2){ll ans=1;for(;p;p>>=1,a=a*a%mod)if(p&1)ans=ans*a%mod;return ans;}
+typedef long double LD;
+const int MAXN=2e5+10;
+inline void solve()
+{
+    int n,m;cin>>n>>m;
+	vii F(n+1,vi(n+1)),g=F;
+	F[1][1]=1;
+	for(int i=1;i<=m;i++)
+	{
+		F.swap(g);
+		F.assign(n+1,vi(n+1));
+		for(int j=1;j<=n;j++)
+			for(int k=1;k<=j;k++)
+			{
+				auto u=g[j][k];
+				if(j<n)MOD(F[j+1][k]+=mul(u,n-j));
+				MOD(F[j][k]+=mul(u,j-k));
+				MOD(F[j][j]+=mul(u,k));
+			}
+	}
+	cout<<F[n][n]<<endl;
+}
+int main()
+{
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);cout<<fixed<<setprecision(10);
+	int T=1;
+	//cin>>T;
+	while(T--)solve();
+	return 0;
+} 
